a3879af09336b819b3a6035d43d56f24b1b437bd
Kamil_Kwiatkowski <53965348+Kamil-git@users.noreply.github.com>
2022-03-31 12:13:15 +0200

    Update index.js

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -15,40 +15,45 @@ x.forEach((x) => {
     
     switch (key) {
       case "w":
+        let crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+       
+
+      case "a":
+        let kickBass = new Audio("sounds/kick-bass.mp3");
+        kickBass.play();
+        break;
+       
+
+      case "s":
+        let snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+       
+
+      case "d":
         let tom1 = new Audio("sounds/tom-1.mp3");
         tom1.play();
         break;
+        
 
-      case "a":
+      case "j":
         let tom2 = new Audio("sounds/tom-2.mp3");
         tom2.play();
         break;
 
-      case "s":
+      case "k":
         let tom3 = new Audio("sounds/tom-3.mp3");
         tom3.play();
         break;
+        
 
-      case "d":
+      case "l":
         let tom4 = new Audio("sounds/tom-4.mp3");
         tom4.play();
         break;
-
-      case "j":
-        let snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        console.log("you pressed j");
-        break;
-
-      case "k":
-        let crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "l":
-        let kickBass = new Audio("sounds/kick-bass.mp3");
-        kickBass.play();
-        break;
+         
       
         default:
         console.log(key);
